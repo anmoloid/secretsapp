80ad89b71730868f31159a07ebf7b14f254157b4
Anmol Agarwal <anmolagarwal@Anmols-MacBook-Pro.local>
2023-04-09 23:13:33 +0530

    Authentication using passport.js package

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
-require("dotenv").config
+require("dotenv").config();
 const express=require("express")
 const bodyParser=require("body-parser")
 const ejs=require("ejs")
 const mongoose=require("mongoose")
-const encrypt=require("mongoose-encryption")
+const session=require("express-session")
+const passport=require("passport")
+const passportLocalMongoose=require("passport-local-mongoose")
+
+const app=express()
+app.set("view engine","ejs")
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+const secret1=process.env.SECRET
+
+app.use(session({
+    secret: secret1,
+    resave: false,
+    saveUninitialized: false
+  }));
+
+//passport is a package for managing cookies and sessions.
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true})
-
-
 
 const userSchema= new mongoose.Schema({
     email: String,
     password: String
 })
-const secret="Thisismylitttlesecretkey.";
+
 //this ensures that whatever user passwords we are creating are stored in a encrypted format. By default it encrypts the whole database,
 //by mentioning the field name we have limited the encryption to only password field
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+//this is the level 2 type security when we are storing encrypted files
+// userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+
+//This plugin is used to hash and salt password and save users in mongodb
+userSchema.plugin(passportLocalMongoose);
 
 const User= new mongoose.model("User",userSchema)
 
-const app=express()
+//learn what these lines of code from the passport documentation. But in short 
+//it basically create a local strategy for the user data and then creates a cookie for storing user data- serialisation
+//and then crumbles the cookie to read the user data
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
-app.set("view engine","ejs")
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static("public"))
 
 app.get("/",function(req,res){
     res.render("home")
@@ -33,38 +57,66 @@ app.get("/",function(req,res){
 app.get("/login",function(req,res){
     res.render("login")
 })
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets")
+    }
+    else{
+        res.redirect("/login")
+    }
+    
+})
 app.get("/register",function(req,res){
     res.render("register")
 })
+app.get("/logout",function(req,res){
+    req.logout(function(err){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.redirect("/");
+        }
+    });
+    
+})
 
 app.post("/register",function(req,res){
-   const user= new User ({
-    email: req.body.username,
-    password: req.body.password
-   }) 
-    User.create(user).then(function(userDetails){
-        res.render("secrets")
-    }).catch(function(error){
-        res.send("<h1>Some error while creating user</h1>")
+    User.register({username:req.body.username},req.body.password,function(err,user){
+        if(err)
+        {
+            console.log(err)
+            res.redirect("/register")
+        }
+        else(
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets")
+            })
+        )
     })
+    
 })
 
 app.post("/login",function(req,res){
+    const user=new  User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-   User.findOne({email:req.body.username}).then(function(results){
-    if(results)
-    {
-    
-       if(results.password === req.body.password) 
-       {
-        res.render("secrets")
-       }
-       else{
-        res.send("<h1>Wrong Password</h1>")
-       }
-    }
-    
-   })
+    req.login(user, function(err){
+        if(err)
+        {
+            console.log(err)
+            res.redirect("/login")
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets")
+        })
+    }})
+
+
+  
 })
 
 app.listen(process.env.PORT || 3000, function(req,res){
